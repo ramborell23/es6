ae38758d432202c076e64fd5109717a02619bdf3
ramborell23 <jerelldavis@ac.c4q.nyc>
2017-11-13 14:36:23 -0500

    First and second exercise

---
+++ es6.js
@@ -0,0 +1,6 @@
+//Ex1
+var map = [1,2,3,4,5,6,7]
+
+
+map.forEach(element => console.log(element))
+    
---
+++ ex2.js
@@ -0,0 +1,10 @@
+
+//Greater than 10
+
+const greaterThan = (arr) => {
+    return arr.filter(num => num > 10 );
+}
+
+
+
+console.log(greaterThan([1,10,11,12,13,1,14]))
