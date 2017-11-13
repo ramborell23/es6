e3970fc6624dcf7fe6273d1d2bdb6999cf7284b2
ramborell23 <jerelldavis@ac.c4q.nyc>
2017-11-13 17:17:10 -0500

    Exercise #11

--- ex11.js
+++ ex11.js
@@ -1,7 +1,32 @@
 
 
-const total = arr =>{
-    return arr.reduce((sum , num) =>sum - num )
+// const total = arr => {
+//     let empt = []
+//     return arr.reduce((sum, num)) 
+
+
+// }
+
+// console.log(total([10, 0, 100, 1, -100, 20, 33]))
+
+
+// const reduce  = arr => 
+//     arr.reduce((min,num) => {
+//         if (num < min ){
+
+//         }
+//     }
+
+const max = (num1, num2) => {
+    if (num1 > num2) {
+        return num1
+    }else {
+     return num2
+   }
 }
 
-console.log(total([10, 0, 100, 1, -100, 20, 33]))
+const reduceMin = arr =>
+    arr.reduce((min, num) => {
+        console.log(`min: ${min}, num: ${num}`)
+        return num < min ? num : min
+    })
